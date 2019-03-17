c758abe0c45426624b5c95b0d1281f2df22df989
ahmadshofiehilmi <ahmadshofiehilmi@gmail.com>
2019-03-17 19:26:29 +0700

    food 1

---
+++ app/src/main/java/com/hunter/myapp/Common/Common.java
@@ -0,0 +1,11 @@
+package com.hunter.myapp.Common;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.hunter.myapp.Model.User;
+
+public class Common {
+    public static User currentUser;
+}
